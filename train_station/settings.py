67c8ec8ef8b2d0badae08d78d2f8311150edfc10
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-11 10:39:13 +0300

    Add initialized apps to installed apps in settings.py

--- train_station/settings.py
+++ train_station/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # my apps
+    "station_api",
+    "user_api",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = "train_station.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
