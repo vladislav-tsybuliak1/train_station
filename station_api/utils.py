6d1f46d518b8bb5b3723ea87b3c8670dce99eae1
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-13 02:28:31 +0300

    Fix train image file path directory name

--- station_api/utils.py
+++ station_api/utils.py
@@ -9,4 +9,4 @@ from station_api.models import Train
 def train_image_file_path(train: Train, filename: str) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(train.name)}-{uuid.uuid4()}{extension}"
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/trains/", filename)
