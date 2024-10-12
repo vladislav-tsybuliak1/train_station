f8c50963da0949a42f6fe684de36706c9fca9f8f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-12 21:51:45 +0300

    Add Route model

--- station_api/models.py
+++ station_api/models.py
@@ -1,10 +1,12 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from station_api.validators import validate_latitude, validate_longitude
 
 
 class Station(models.Model):
-    name = models.CharField(max_length=63)
+    name = models.CharField(max_length=63, unique=True)
     latitude = models.FloatField(validators=[validate_latitude])
     longitude = models.FloatField(validators=[validate_longitude])
 
@@ -13,3 +15,38 @@ class Station(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        related_name="source_routes",
+        on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Station,
+        related_name="destination_routes",
+        on_delete=models.CASCADE
+    )
+
+    distance = models.IntegerField(validators=[MinValueValidator(0)])
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.destination}"
+
+    @staticmethod
+    def validate_different_stations(
+            source: Station,
+            destination: Station
+    ) -> None:
+        if source == destination:
+            raise ValidationError(
+                "The source and destination stations must be different."
+            )
+
+    def clean(self) -> None:
+        Route.validate_different_stations(self.source, self.destination)
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
