81c3fd8552fc99814be23ae0f8c9a32325cdcf99
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-12 11:42:45 +0300

    Add Station model

--- station_api/models.py
+++ station_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from station_api.validators import validate_latitude, validate_longitude
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=63)
+    latitude = models.FloatField(validators=[validate_latitude])
+    longitude = models.FloatField(validators=[validate_longitude])
+
+    def __str__(self) -> str:
+        return self.name
