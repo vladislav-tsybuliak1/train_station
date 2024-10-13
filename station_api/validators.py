7cc4ac17ecc5185a9d22a884990bde7e3cbbcf83
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-13 21:29:21 +0300

    Fix validate latitude and longitude validators

--- station_api/validators.py
+++ station_api/validators.py
@@ -4,12 +4,12 @@ from django.core.exceptions import ValidationError
 
 
 def validate_latitude(value: float) -> None:
-    if -90 < value < 90:
+    if not -90 < value < 90:
         raise ValidationError("Latitude must be between -90 and 90 degrees.")
 
 
 def validate_longitude(value: float) -> None:
-    if -180 < value < 180:
+    if not -180 < value < 180:
         raise ValidationError(
             "Longitude must be between -180 and 180 degrees."
         )
