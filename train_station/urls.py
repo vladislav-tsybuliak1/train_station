7e4688fd532edf96175bdb1e595fc9cda5b807f6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-13 20:57:55 +0300

    Add endpoint for station_api

--- train_station/urls.py
+++ train_station/urls.py
@@ -18,10 +18,13 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/train-station/", include("station_api.urls", namespace="station-api"))
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += debug_toolbar_urls()
