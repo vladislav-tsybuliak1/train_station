f3ec9d3465814cd35ed795148774ecffd411a30c
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-13 20:54:26 +0300

    Fix train image file path

--- station_api/utils.py
+++ station_api/utils.py
@@ -3,10 +3,8 @@ import uuid
 
 from django.utils.text import slugify
 
-from station_api.models import Train
 
-
-def train_image_file_path(train: Train, filename: str) -> str:
+def train_image_file_path(train, filename) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(train.name)}-{uuid.uuid4()}{extension}"
     return os.path.join("uploads/trains/", filename)
