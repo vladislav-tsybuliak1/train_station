8a9f2ff6b334e49362fc15bb4b686ff471b5bb95
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-14 23:13:47 +0300

    Register models to the admin panel

--- station_api/admin.py
+++ station_api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from station_api.models import (
+    Station,
+    Route,
+    Crew,
+    TrainType,
+    Train,
+    Trip,
+    Ticket,
+    Order,
+)
+
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(TrainType)
+admin.site.register(Train)
+admin.site.register(Trip)
+admin.site.register(Ticket)
+admin.site.register(Order)
