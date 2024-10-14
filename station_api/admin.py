1e44f7b8d93c38282331672b470428b6ae5cc572
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-14 23:41:57 +0300

    Add inlines for Order to the Order model in the admin panel

--- station_api/admin.py
+++ station_api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-
 from station_api.models import (
     Station,
     Route,
@@ -12,6 +11,17 @@ from station_api.models import (
     Order,
 )
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Crew)
@@ -19,4 +29,3 @@ admin.site.register(TrainType)
 admin.site.register(Train)
 admin.site.register(Trip)
 admin.site.register(Ticket)
-admin.site.register(Order)
