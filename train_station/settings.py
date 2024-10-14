28ccb1930046ef3ce35bfdd30fa25fe6bd34339b
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-15 00:34:24 +0300

    Add default throttle classes and settings for them

--- train_station/settings.py
+++ train_station/settings.py
@@ -160,9 +160,17 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "station_api.permissions.IsAdminOrIfAuthenticatedReadOnly"
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minute",
+        "user": "30/minute"
+    }
 }
 
 
