9dd313798881c082fb0ae971a54713e57b3334e8
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-14 12:15:43 +0300

    Add deletion of train images from media after train is deletede

--- station_api/signals.py
+++ station_api/signals.py
@@ -3,10 +3,16 @@ import os
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-from station_api.models import Crew
+from station_api.models import Crew, Train
 
 
 @receiver(pre_delete, sender=Crew)
 def delete_crew_image(sender, instance, **kwargs) -> None:
     if os.path.isfile(instance.crew_image.path):
         os.remove(instance.crew_image.path)
+
+
+@receiver(pre_delete, sender=Train)
+def delete_crew_image(sender, instance, **kwargs) -> None:
+    if os.path.isfile(instance.train_image.path):
+        os.remove(instance.train_image.path)
