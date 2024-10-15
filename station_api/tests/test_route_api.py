2ede4de93c548ab00b778ee24d5de191e94ca3fc
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-15 22:18:28 +0300

    Add tests for admin route api

--- station_api/tests/test_route_api.py
+++ station_api/tests/test_route_api.py
@@ -137,3 +137,87 @@ class AuthenticatedRouteApiTests(TestCase):
     def test_delete_route_forbidden(self) -> None:
         response = self.client.delete(detail_url(self.route_1.id))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminRouteApiTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test@test.com",
+            password="testpassword",
+            is_staff=True
+        )
+        self.client.force_authenticate(self.user)
+
+        self.station_1 = Station.objects.create(
+            name="Chernivtsi",
+            latitude=48.29,
+            longitude=25.39
+        )
+        self.station_2 = Station.objects.create(
+            name="Donetsk",
+            latitude=48.01,
+            longitude=37.81
+        )
+
+        self.route_1 = Route.objects.create(
+            source=self.station_1,
+            destination=self.station_2,
+            distance=950
+        )
+
+        self.payload = {
+            "source": self.station_2.id,
+            "destination": self.station_1.id,
+            "distance": 650
+        }
+
+    def test_create_route(self) -> None:
+        response = self.client.post(ROUTE_URL, self.payload)
+        route = Route.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.payload["source"], route.source.id)
+        self.assertEqual(self.payload["destination"], route.destination.id)
+        self.assertEqual(self.payload["distance"], route.distance)
+
+    def test_create_route_same_source_and_destination(self) -> None:
+        self.payload["source"] = self.station_1.id
+        self.payload["destination"] = self.station_1.id
+        response = self.client.post(ROUTE_URL, self.payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_route_with_negative_distance(self) -> None:
+        self.payload["distance"] = -100
+        response = self.client.post(ROUTE_URL, self.payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_route(self) -> None:
+        route = self.route_1
+        response = self.client.put(detail_url(route.id), self.payload)
+        route.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.payload["source"], route.source.id)
+        self.assertEqual(self.payload["destination"], route.destination.id)
+        self.assertEqual(self.payload["distance"], route.distance)
+
+    def test_partial_update_route(self) -> None:
+        route = self.route_1
+        response = self.client.patch(detail_url(route.id), self.payload)
+        route.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.payload["source"], route.source.id)
+        self.assertEqual(self.payload["destination"], route.destination.id)
+        self.assertEqual(self.payload["distance"], route.distance)
+
+    def test_delete_route(self) -> None:
+        route = self.route_1
+        response = self.client.delete(detail_url(route.id))
+        route_exists = Route.objects.filter(id=route.id).exists()
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(route_exists)
