1d747283b31bdb53b99dac93a2600e9c65c515fc
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-10-16 03:09:58 +0300

    Add secret key do .env

--- train_station/settings.py
+++ train_station/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6t+!(sztm#8mwm^p6_0-ue7ca(p*775_dj-a4_6vvat8f_(*jl"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
